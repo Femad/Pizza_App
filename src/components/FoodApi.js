b57fcc33ac306f7b38553087af36b48f95e4e236
Femad <Femadkon@gmail.com>
2023-11-07 15:59:49 +0100

    .

--- src/components/FoodApi.js
+++ src/components/FoodApi.js
@@ -1,9 +1,23 @@
+import img01 from "../assets/01.jpg";
+import img02 from "../assets/02.jpg";
+import client1 from "../assets/client1.jpg";
+import client2 from "../assets/client2.jpg";
+
+import f1 from "../assets/f1.png";
+import f2 from "../assets/f2.png";
+import f3 from "../assets/f3.png";
+import f4 from "../assets/f4.png";
+import f5 from "../assets/f5.png";
+import f6 from "../assets/f6.png";
+import f7 from "../assets/f7.png";
+import f8 from "../assets/f8.png";
+import f9 from "../assets/f9.png";
 export const mealData = [
   {
     id: 1,
     category: "pasta",
     name: "Delicious Pasta",
-    image: "/src/assets/f4.png",
+    image: f4,
     price: 1800,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -12,7 +26,7 @@ export const mealData = [
     id: 2,
     category: "pizza",
     name: "Delicious Pizza",
-    image: "/src/assets/f1.png",
+    image: f1,
     price: 2000,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -21,7 +35,7 @@ export const mealData = [
     id: 3,
     category: "pizza",
     name: "Delicious Pizza",
-    image: "/src/assets/f3.png",
+    image: f3,
     price: 1700,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -30,7 +44,7 @@ export const mealData = [
     id: 4,
     category: "fries",
     name: "French Fries",
-    image: "/src/assets/f5.png",
+    image: f5,
     price: 1000,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -39,7 +53,7 @@ export const mealData = [
     id: 5,
     category: "pizza",
     name: "Delicious Pizza",
-    image: "/src/assets/f6.png",
+    image: f6,
     price: 1500,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -48,7 +62,7 @@ export const mealData = [
     id: 6,
     category: "burger",
     name: "Tasty Burger",
-    image: "/src/assets/f7.png",
+    image: f7,
     price: 1200,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -57,7 +71,7 @@ export const mealData = [
     id: 7,
     category: "burger",
     name: "Tasty Burger",
-    image: "/src/assets/f8.png",
+    image: f8,
     price: 1400,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -66,7 +80,7 @@ export const mealData = [
     id: 8,
     category: "pasta",
     name: "Delicious Pasta",
-    image: "/src/assets/f9.png",
+    image: f9,
     price: 1000,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -75,7 +89,7 @@ export const mealData = [
     id: 9,
     category: "burger",
     name: "Tasty Burger",
-    image: "/src/assets/f2.png",
+    image: f2,
     price: 1500,
     detailText:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
@@ -87,13 +101,13 @@ export const orderNow = [
     id: 1,
     dayCaption: "Tasty Thursdays",
     amountOff: 20,
-    image: "/src/assets/01.jpg",
+    image: img01,
   },
   {
     id: 2,
     dayCaption: "Pizza Days",
     amountOff: 15,
-    image: "/src/assets/02.jpg",
+    image: img02,
   },
 ];
 export const catButon = [
@@ -116,5 +130,22 @@ export const catButon = [
     id: 4,
     catValue: "fries",
     btnValue: "Fries",
+  },
+];
+
+export const testimonies = [
+  {
+    id: 1,
+    name: "Moana Michell",
+    image: client1,
+    testimonyText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  },
+  {
+    id: 2,
+    name: "Mike Hamell",
+    image: client2,
+    testimonyText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
 ];
